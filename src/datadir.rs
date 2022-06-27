662a446db6381571f5972b5053cfb0668c834940
Steven Michael <ssmichael@gmail.com>
2022-06-27 15:45:53 -0400

    trying search for data directory

---
+++ src/datadir.rs
@@ -0,0 +1,97 @@
+use nix::libc;
+use std::fs::metadata;
+use std::path::Path;
+use std::{ffi::CStr, os::raw::c_void, path::PathBuf};
+
+#[inline]
+pub(crate) fn get_dylib_path() -> Option<PathBuf> {
+    let mut dl_info = libc::Dl_info {
+        dli_fname: core::ptr::null(),
+        dli_fbase: core::ptr::null_mut(),
+        dli_sname: core::ptr::null(),
+        dli_saddr: core::ptr::null_mut(),
+    };
+    if unsafe {
+        libc::dladdr(
+            get_dylib_path as *const c_void,
+            &mut dl_info as *mut libc::Dl_info,
+        ) != 0
+    } {
+        if dl_info.dli_fname.is_null() {
+            None
+        } else {
+            match unsafe { CStr::from_ptr(dl_info.dli_fname) }.to_str() {
+                Ok(path) => Some(PathBuf::from(path)),
+                Err(_) => None,
+            }
+        }
+    } else {
+        None
+    }
+}
+
+#[used]
+#[cfg_attr(target_os = "linux", link_section = ".ctors")]
+#[cfg_attr(target_os = "macos", link_section = "__DATA,__mod_init_func")]
+#[cfg_attr(target_os = "windows", link_section = ".CRT$XCU")]
+static FOO: extern "C" fn() = {
+    extern "C" fn foo() {
+        fn testdir(dir: &str) {
+            println!("dir = {}", dir);
+        }
+
+        let mut testdirs: Vec<PathBuf> = Vec::new();
+
+        // Look for paths under home directory
+        match std::env::var(&"HOME") {
+            Ok(val) => {
+                let vstr = &String::from(val);
+                testdirs.push(Path::new(vstr).to_path_buf());
+                testdirs.push(Path::new(vstr).join("astro-data"));
+                #[cfg(target_os = "macos")]
+                testdirs.push(
+                    Path::new(vstr)
+                        .join("Libaray")
+                        .join("Application Support")
+                        .join("astro-data"),
+                );
+            }
+            Err(_e) => (),
+        }
+
+        // Look for paths in environment variable
+        match std::env::var(&"ASTROLIB_DATA") {
+            Ok(val) => testdirs.push(Path::new(&val).to_path_buf()),
+            Err(_) => (),
+        }
+
+        // Look for paths in current library directory
+        match get_dylib_path() {
+            Some(v) => {
+                testdirs.push(Path::new(&v).join("share").join("astro-data"));
+                testdirs.push(PathBuf::from(&v).join("astro-data"));
+                testdirs.push(v);
+            }
+            None => (),
+        }
+
+        testdirs.push(Path::new(&"/usr/share/astrodata").to_path_buf());
+
+        // On mac, look in root library directory
+        #[cfg(target_os = "macos")]
+        testdirs.push(Path::new(&"/Library/Application Support/astro-data").to_path_buf());
+
+        println!("{:?}", testdirs);
+        let dirname = "/Users/st16626";
+        match metadata(dirname) {
+            Ok(v) => {
+                if v.is_dir() {
+                    testdir(dirname)
+                }
+            }
+            Err(_e) => (),
+        }
+    }
+
+    foo
+};
