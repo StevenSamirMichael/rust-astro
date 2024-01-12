88e525500078c13affbedf2be5df05fad522ec6f
Steven Michael <ssmichael@gmail.com>
2024-01-11 22:04:45 -0500

    add initial hooks for nrl msise 2.1 linking

--- build.rs
+++ build.rs
@@ -1,6 +1,29 @@
 use std::process::Command;
 
 fn main() {
+    let nrlmsise21_files = [
+        "msis_constants.F90",
+        "msis_utils.F90",
+        "msis_init.F90",
+        "msis_gfn.F90",
+        "msis_tfn.F90",
+        "msis_dfn.F90",
+        "msis_calc.F90",
+        "msis_gtd8d.F90",
+    ];
+    cc::Build::new()
+        .files(
+            nrlmsise21_files
+                .iter()
+                .map(|&file| format!("extern/nrlmsise2.1/{}", file)),
+        )
+        .compiler("gfortran")
+        .flag("-std=legacy")
+        .flag("-w")
+        .flag("-O3")
+        .flag("-Jextern/nrlmsise2.1/")
+        .compile("nrlmsise21");
+
     cc::Build::new()
         .file("extern/nrlmsise/nrlmsise-00.c")
         .file("extern/nrlmsise/nrlmsise-00_data.c")
