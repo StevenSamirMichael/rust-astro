1283dad0e938d8a0bd08470cd7ba91bab3db3fa9
Steven Michael <ssmichael@gmail.com>
2024-03-01 09:57:57 -0500

    try more-complex build and test workflow with cache

--- pytest/download_data.py
+++ pytest/download_data.py
@@ -7,10 +7,17 @@ import json
 from pathlib import Path
 import requests
 from download_from_json import download_from_json
+import sys
 
 if __name__ == "__main__":
+
     baseurl = "https://storage.googleapis.com/astrokit-astro-data"
-    basedir = "astro-data"
+
+    if len(sys.argv) > 1:
+        basedir = sys.argv[1]
+    else:
+        basedir = "astro-data"
+
     fileurl = baseurl + "/files.json"
     headers = {"Accept": "application/json"}
 
--- pytest/download_testvecs.py
+++ pytest/download_testvecs.py
@@ -8,10 +8,16 @@ import os
 from pathlib import Path
 import requests
 from download_from_json import download_from_json
+import sys
 
 if __name__ == "__main__":
     baseurl = "https://storage.googleapis.com/satkit-testvecs"
-    basedir = "satkit-testvecs"
+
+    if len(sys.argv) > 1:
+        basedir = sys.argv[1]
+    else:
+        basedir = "satkit-testvecs"
+
     fileurl = baseurl + "/files.json"
     headers = {"Accept": "application/json"}
 
